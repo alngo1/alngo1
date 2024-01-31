3a206ea8d7db000797dfeba2a7db0fbd930354d0
Alex Ngo <42555152+alngo1@users.noreply.github.com>
2024-01-30 17:34:43 -0800

    files moved, diag title movement
    moved navbar links and socials to new css file and made title move diagonally

--- scroll.js
+++ scroll.js
@@ -1,8 +1,19 @@
 export function scroll(){
+
+    function moveTitleAndMonkey(bool, title, monkeyGif){
+        if(bool === true){
+            title.style.transform = "translateY(-100rem) translateX(-100rem)";
+            monkeyGif.style.transform = "translateY(-100rem) translateX(100rem)";
+        }else{
+            title.style.transform = "translateY(0rem)";
+            monkeyGif.style.transform = "translateY(0rem) translateX(0rem)";
+        }
+    }
+
     let moveMonkey = false;
 
     window.addEventListener("scroll", function() {
-        let elementTarget = document.getElementsByClassName("nav-links")[0];
+        let elementTarget = document.getElementsByClassName("top")[0];
 
         let monkeyGif = document.querySelector(".monkey-gif img");
         let title = document.querySelector(".title h1");
@@ -11,14 +22,11 @@ export function scroll(){
             moveMonkey = true;
             console.log(moveMonkey)
             if(moveMonkey === true){
-                title.style.transform = "translateY(-100rem)";
-                monkeyGif.style.transform = "translateY(-100rem)";
+                moveTitleAndMonkey(moveMonkey, title, monkeyGif);
             }
         }else{
-            title.style.transform = "translateY(0rem)";
-            monkeyGif.style.transform = "translateY(0rem)";
-
             moveMonkey = false;
+            moveTitleAndMonkey(moveMonkey, title, monkeyGif);
         }
     });
 }
