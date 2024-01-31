6badd9dd4d1084e11c403d282da4c6b99f9d882a
Alex Ngo <42555152+alngo1@users.noreply.github.com>
2024-01-30 22:21:40 -0800

    added navbar, title, and monkey transitions

--- scroll.js
+++ scroll.js
@@ -1,32 +1,53 @@
 export function scroll(){
 
-    function moveTitleAndMonkey(bool, title, monkeyGif){
-        if(bool === true){
-            title.style.transform = "translateY(-100rem) translateX(-100rem)";
-            monkeyGif.style.transform = "translateY(-100rem) translateX(100rem)";
-        }else{
-            title.style.transform = "translateY(0rem)";
-            monkeyGif.style.transform = "translateY(0rem) translateX(0rem)";
-        }
+    //helper functions to change transforms, transitions not set here are set in the css files
+    function scrollDownAnimation(){
+        //animate title
+        let title = document.querySelector(".title h1");
+        title.style.transform = "translateY(-1000px)";
+
+        //animate monkey gif
+        let monkeyGif = document.querySelector(".monkey-gif img");
+        monkeyGif.style.transform = "translateY(-1000px)";
+
+        //animate nav-title
+        let nav_title = document.querySelector(".nav-title");
+        nav_title.style.opacity = "1";
+        nav_title.style.transform = "scale(1)";
+        nav_title.style.transition = nav_transition;
     }
 
+    function scrollUpAnimation(){
+        //animate title
+        let title = document.querySelector(".title h1");
+        title.style.transform = "translateY(0em)";
+
+        //animate monkey gif
+        let monkeyGif = document.querySelector(".monkey-gif img");
+        monkeyGif.style.transform = "translateY(0em)";
+
+        //animate nav-title
+        let nav_title = document.querySelector(".nav-title");
+        nav_title.style.opacity = "0";
+        nav_title.style.transform = "scale(0.5)";
+        nav_title.style.transition = "all 0.2s ease-in 0s";
+    }
+
+
     let moveMonkey = false;
+    let nav_title = document.querySelector(".nav-title");
+    let nav_transition = nav_title.style.transition
 
     window.addEventListener("scroll", function() {
         let elementTarget = document.getElementsByClassName("top")[0];
 
-        let monkeyGif = document.querySelector(".monkey-gif img");
-        let title = document.querySelector(".title h1");
-
         if(this.window.scrollY >= (elementTarget.offsetTop + elementTarget.offsetHeight)){
             moveMonkey = true;
             console.log(moveMonkey)
-            if(moveMonkey === true){
-                moveTitleAndMonkey(moveMonkey, title, monkeyGif);
-            }
+            scrollDownAnimation()
         }else{
             moveMonkey = false;
-            moveTitleAndMonkey(moveMonkey, title, monkeyGif);
+            scrollUpAnimation()
         }
     });
 }
